56f8071201a8f245de4116bafe364f5c4eaa706b
mzappacosta <mzappacosta@ivolutia.com>
2023-06-05 09:28:14 -0300

    Adding unit tests

--- notes/packages/functions/src/create.ts
+++ notes/packages/functions/src/create.ts
@@ -12,7 +12,7 @@ export async function main(event) {
     TableName: Table.Notes.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      uuserId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
